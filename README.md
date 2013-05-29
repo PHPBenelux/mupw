2dbaad1f607561f5cdc49b96b4ddfc5957192980
Michelangelo <dragonbe+github@gmail.com>
2013-05-29 21:15:52 +0200

    And now we can raffle meetup meetings

---
+++ README.md
@@ -0,0 +1,9 @@
+# MUPW
+
+TekHack is a simple tool to randomly pick and choose a winner from all
+participants in the [php[tek] 2013 hackathon](https://www.facebook.com/events/364378906997047)
+on [Facebook](https://www.facebook.com).
+
+By participating, you automatically have a chance to win a great prize.
+
+The prize raffle tool can be found at [http://tekhack.azurewebsites.net](http://tekhack.azurewebsites.net).
--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -10,7 +10,29 @@ class IndexController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        // action body
+        $config = new Zend_Config_Ini(APPLICATION_PATH . '/configs/api.ini');
+
+        $eventId = $this->getRequest()->getParam('event', 115919622);
+        $url = sprintf('https://api.meetup.com/2/rsvps?&sign=true&event_id=%d&page=20&key=%s',
+            $eventId, $config->meetup->api->key);
+        $client = new Zend_Http_Client($url);
+
+        $response = $client->request(Zend_Http_Client::GET);
+
+        $data = json_decode($response->getBody());
+
+        $attendees = array ();
+        foreach ($data->results as $member) {
+            if ('yes' === $member->response) {
+                $attendees[] = $member;
+            }
+        }
+
+
+        $this->view->assign(array (
+            'response' => $attendees,
+            'eventId' => $eventId,
+        ));
     }
 
 
